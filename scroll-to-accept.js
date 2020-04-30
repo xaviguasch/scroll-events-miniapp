527fddfbae3b7b646a6e370064f8626222716c6e
xaviguasch <xaviguasch@gmail.com>
2020-04-30 23:13:24 +0200

    added observer implementation

--- scroll-to-accept.js
+++ scroll-to-accept.js
@@ -1,10 +1,15 @@
 const terms = document.querySelector('.terms-and-conditions')
 const watch = document.querySelector('.watch')
+const button = document.querySelector('.accept')
 
 function obCallback(payload) {
-  console.log(payload)
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild)
+  }
 }
 
-const ob = new IntersectionObserver(obCallback)
+const ob = new IntersectionObserver(obCallback, { root: terms, threshold: 1 })
 
-ob.observe(watch)
+ob.observe(terms.lastElementChild)
